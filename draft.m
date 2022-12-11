e7356fe4ff499c5ac628bddc70442b4d3da350e7
Raphael Haehnel <raphael.haehnel@gmail.com>
2022-12-11 23:25:41 +0200

    update github

--- draft.m
+++ draft.m
@@ -32,9 +32,9 @@ fig = gca;
 simulate_wave(3, 0, linspace(-5, 5, 100), linspace(-5, 5, 100), wave1+wave2, fig);
 
 %% Unit test 4
-wave1 = Wave(pi/4, 30, 1, "Gaussian");
-wave2 = Wave(-pi/4, 30, 1, "Gaussian");
+wave1 = Wave(pi/4, 20, 1, "Cylindrical");
+wave2 = Wave(-pi/4, 20, 1, "Gaussian");
 
 figure;
 fig = gca;
-create_video_wave(10, 70, 60, linspace(-5, 5, 300), linspace(-5, 5, 300), wave1+wave2, fig);
+create_video_wave(10, 70, 40, linspace(-5, 5, 300), linspace(-5, 5, 300), wave1+wave2, fig);
